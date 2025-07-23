9db01bea15002cb8f11d37c9ec239cf21c5dccd7
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-23 00:17:31 -0400

    feat: a more useful date format

--- fish/.config/fish/conf.d/bobthefish.fish
+++ fish/.config/fish/conf.d/bobthefish.fish
@@ -1,2 +1,5 @@
 # https://github.com/oh-my-fish/theme-bobthefish?tab=readme-ov-file#configuration
 set -g theme_newline_cursor yes
+
+# https://deepwiki.com/oh-my-fish/theme-bobthefish/5.1-right-prompt#configuration-variables
+set -g theme_date_format "+%T.%N"
