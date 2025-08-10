6bebee0e44e6ff149b728911d63042dd2be01341
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-10 03:15:41 -0400

    refactor:🔨 change default date format in right prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ set --path PATH $HOME/bin $PATH
 if status is-interactive
     # fish_vi_key_bindings
     set -g theme_newline_cursor yes
-    set -g theme_date_format "+%T.%N"
+    set -g theme_date_format "+%s"
     set -g theme_display_git_untracked no
     clear
 end
