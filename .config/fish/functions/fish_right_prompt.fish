dfafcdfa6d28a1be44d7d40cdaead2450ab1e2b4
Marcelo de Freitas Andrade <mfandrad@motorola.com>
2025-08-11 12:09:05 -0400

    style:💅 right prompt is a problem to copy commands

---
+++ .config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,3 @@
+# Defined in ~/.local/share/omf/themes/bobthefish/functions/fish_right_prompt.fish @ line 69
+function fish_right_prompt --description 'bobthefish is all about the right prompt'
+end
