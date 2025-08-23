06078680071bc68a8e658f5e041ec852183a9c94
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-22 23:31:58 -0400

    refactor: remove omf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,11 +12,7 @@ if status is-interactive
     end
 
     set -x EZA_ICONS_AUTO yes
-    set -g theme_newline_cursor yes
-    set -g theme_date_format "+%s"
-    set -g theme_display_git_untracked no
-    set -g theme_show_exit_status no
-    # fish_vi_key_bindings; set -g theme_display_vi yes
+    # fish_vi_key_bindings # set -g theme_display_vi yes
 
     if test -z "$TMUX"; and type -q tmux
         tmux attach -t default 2>/dev/null; or tmux new -s default
