993a0443e77513b751238c7a2321d89ccaf969a8
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-22 23:37:39 -0400

    refactor: reinstall bobthefish

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -67,7 +67,7 @@ function __bobthefish_timestamp -S -d 'Show the current timestamp'
 end
 
 function fish_right_prompt -d 'bobthefish is all about the right prompt'
-    set -l __bobthefish_left_arrow_glyph \u276E #\uE0B3
+    set -l __bobthefish_left_arrow_glyph \uE0B3
     if [ "$theme_powerline_fonts" = no -a "$theme_nerd_fonts" != yes ]
         set __bobthefish_left_arrow_glyph '<'
     end
